c95cfbd3316fa2a2be26a8d921421fcf4142aa4b
Erick Sanchez <efs3@njit.edu>
2012-06-20 20:51:50 +0000

    Reference 500 error express, connection to mongo db stablished

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 require('coffee-script');
 
+
 /**
  * Module dependencies.
  */
 
 var express = require('express'),
-		RedisStore = require('connect-redis')(express);
+		RedisStore = require('connect-redis')(express),
+    mongoose = require('mongoose'); 
+//creates the mongo connection   
+var connection = mongoose.createConnection('mongodb://localhost/pla');    
+
+//initializzes an empty schema object
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId;
+//populates the schema object with properties  
+var User = new Schema({
+  login     : String,
+  password  : String
+});
+//UserModel is an object that sets itself to work with the users collection and the user schema defined above
+var UserModel = connection.model('users', User);
+//this creates a new empty user model
+	var user = new UserModel();
+		user.login = 'keith';
+		user.password = 'keith$1234';
+//this saves the new user
+	user.save();	
+
+//this gets one user i.e. keith
+UserModel.findOne({login : 'keith'}, function (err,doc) {
+	console.log(doc);
+});
 
 require('express-namespace');
 
